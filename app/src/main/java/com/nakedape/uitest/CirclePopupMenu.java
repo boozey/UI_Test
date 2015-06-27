01bc38b3519758baa16f1c2c2278623f399cf712
boozey <lastplace@hotmail.com>
2015-06-26 20:50:09 -0700

    Unfinished

--- app/src/main/java/com/nakedape/uitest/CirclePopupMenu.java
+++ app/src/main/java/com/nakedape/uitest/CirclePopupMenu.java
@@ -1,7 +1,14 @@
 package com.nakedape.uitest;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -10,90 +17,118 @@ import android.widget.RelativeLayout;
  */
 public class CirclePopupMenu extends RelativeLayout {
 
-    public ImageView center, topLeft, top, topRight, left, right, bottomLeft, bottom, bottomRight;
+    public Button center, topLeft, top, topRight, left, right, bottomLeft, bottom, bottomRight;
     private Context context;
-    private int radius = 100;
-    private int itemWidth = 60, itemHeight = 60;
+    private int radius = 120;
+    private int center_diam = 100, item_diam = 60;
 
     public CirclePopupMenu(Context context, AttributeSet attrs){
         super(context, attrs);
         this.context = context;
-        setMinimumWidth(300);
-        setMinimumHeight(300);
-        center = new ImageView(context);
+        setVisibility(GONE);
+        setMinimumWidth(radius + 2 * item_diam);
+        setMinimumHeight(radius + 2 * item_diam);
+        center = new Button(context);
         center.setBackground(getResources().getDrawable(R.drawable.circle_popup_center));
         addView(center);
-        topLeft = new ImageView(context);
+        topLeft = new Button(context);
         topLeft.setBackground(getResources().getDrawable(R.drawable.circle_popup_item));
         addView(topLeft);
-        top = new ImageView(context);
+        top = new Button(context);
         top.setBackground(getResources().getDrawable(R.drawable.circle_popup_item));
         addView(top);
-        topRight = new ImageView(context);
+        topRight = new Button(context);
         topRight.setBackground(getResources().getDrawable(R.drawable.circle_popup_item));
         addView(topRight);
-        right = new ImageView(context);
+        right = new Button(context);
         right.setBackground(getResources().getDrawable(R.drawable.circle_popup_item));
         addView(right);
-        bottomRight = new ImageView(context);
+        bottomRight = new Button(context);
         bottomRight.setBackground(getResources().getDrawable(R.drawable.circle_popup_item));
         addView(bottomRight);
-        bottom = new ImageView(context);
+        bottom = new Button(context);
         bottom.setBackground(getResources().getDrawable(R.drawable.circle_popup_item));
         addView(bottom);
-        bottomLeft = new ImageView(context);
+        bottomLeft = new Button(context);
         bottomLeft.setBackground(getResources().getDrawable(R.drawable.circle_popup_item));
         addView(bottomLeft);
-        left = new ImageView(context);
+        left = new Button(context);
         left.setBackground(getResources().getDrawable(R.drawable.circle_popup_item));
         addView(left);
+        for (int i = 1; i < getChildCount(); i++){
+            getChildAt(i).setOnTouchListener(itemTouchListener);
+        }
+        this.setOnTouchListener(itemTouchListener);
     }
-
 
     @Override
-    protected void onSizeChanged (int w, int h, int oldw, int oldh) {
-        PositionViews(w, h);
-        super.onSizeChanged(w, h, oldw, oldh);
+    protected void onMeasure (int widthMeasureSpec, int heightMeasureSpec){
+        int diam = 2 * radius + item_diam;
+        setMeasuredDimension(diam, diam);
     }
-    private void PositionViews(int w, int h){
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(120, 120);
-        int xOffset = (itemWidth) / 2, yOffset = (itemHeight) / 2;
-        params.setMargins(w / 2 - 60, h / 2 - 60, 0, 0);
-        center.setLayoutParams(params);
 
-        int originX = w / 2, originY = h / 2;
-        int a = -(int)Math.sqrt(radius*radius / 2);
+    @Override
+    protected void onLayout (boolean changed, int left, int top, int right, int bottom){
+        if (changed){
+            int w = right - left, h = bottom - top;
+            int originX = w / 2, originY = h / 2;
+            int a = (int)Math.sqrt(radius*radius / 2);
+            getChildAt(0).layout(originX - center_diam / 2, originY - center_diam / 2, originX + center_diam / 2, originY + center_diam / 2);
 
-        params = new RelativeLayout.LayoutParams(itemWidth, itemHeight);
-        params.setMargins(originX - a - xOffset, originY + a - yOffset, 0, 0);
-        topLeft.setLayoutParams(params);
-
-        params = new RelativeLayout.LayoutParams(itemWidth, itemHeight);
-        params.setMargins(originX - xOffset, originY - radius - yOffset, 0, 0);
-        top.setLayoutParams(params);
-
-        params = new RelativeLayout.LayoutParams(itemWidth, itemHeight);
-        params.setMargins(originX + a - xOffset, originY + a - yOffset, 0, 0);
-        topRight.setLayoutParams(params);
-
-        params = new RelativeLayout.LayoutParams(itemWidth, itemHeight);
-        params.setMargins(originX + radius - xOffset, originY - yOffset, 0, 0);
-        right.setLayoutParams(params);
-
-        params = new RelativeLayout.LayoutParams(itemWidth, itemHeight);
-        params.setMargins(originX + a - xOffset, originY - a - yOffset, 0, 0);
-        bottomRight.setLayoutParams(params);
-
-        params = new RelativeLayout.LayoutParams(itemWidth, itemHeight);
-        params.setMargins(originX - xOffset, originY + radius - yOffset, 0, 0);
-        bottom.setLayoutParams(params);
-
-        params = new RelativeLayout.LayoutParams(itemWidth, itemHeight);
-        params.setMargins(originX - a - xOffset, originY - a - yOffset, 0, 0);
-        bottomLeft.setLayoutParams(params);
-
-        params = new RelativeLayout.LayoutParams(itemWidth, itemHeight);
-        params.setMargins(originX - radius - xOffset, originY - yOffset, 0, 0);
-        left.setLayoutParams(params);
+            int xOffset = (item_diam) / 2, yOffset = (item_diam) / 2;
+            originX = w / 2 - xOffset; originY = h / 2 - yOffset;
+            getChildAt(1).layout(originX - a, originY - a, originX - a + item_diam, originY - a + item_diam);
+            getChildAt(2).layout(originX, originY - radius, originX + item_diam, originY - radius + item_diam);
+            getChildAt(3).layout(originX + a, originY - a, originX + a + item_diam, originY - a + item_diam);
+            getChildAt(4).layout(originX + radius, originY, originX + radius + item_diam, originY - a + item_diam);
+        }
     }
+
+    private int getPredictedWidth(){
+        if (getMeasuredWidth() == 0)
+            return 2 * radius + item_diam;
+        else
+            return getMeasuredWidth();
+    }
+
+    public void ShowAt(int x, int y, MotionEvent event) {
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.setMargins(x - getPredictedWidth() / 2, y - getPredictedWidth() / 2, 0, 0);
+        setLayoutParams(params);
+        setVisibility(View.VISIBLE);
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(context,
+                R.animator.animator_circle_popup_item_highlight);
+        set.setTarget(this);
+        set.start();
+        super.clearFocus();
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        requestFocus();
+    }
+
+    private OnTouchListener itemTouchListener = new OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            Log.d("CirclePopupMenu", "touch event");
+            AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(context,
+                    R.animator.animator_circle_popup_item_highlight);
+            set.setTarget(v);
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    set.start();
+                    if (v.equals(getChildAt(0)))
+                        return false;
+                    else
+                        return true;
+                case MotionEvent.ACTION_MOVE:
+                    set.start();
+                    return true;
+                case MotionEvent.ACTION_UP:
+                    setVisibility(GONE);
+                default:
+                    return true;
+            }
+        }
+    };
 }
--- app/src/main/java/com/nakedape/uitest/MainActivity.java
+++ app/src/main/java/com/nakedape/uitest/MainActivity.java
@@ -1,19 +1,50 @@
 package com.nakedape.uitest;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private RelativeLayout background;
+    private Context context;
+    private CirclePopupMenu popupMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+        background = (RelativeLayout)findViewById(R.id.background);
+        background.setOnTouchListener(bgLongClickListener);
+        popupMenu = new CirclePopupMenu(context, null);
+        background.addView(popupMenu);
     }
+
+    private View.OnTouchListener bgLongClickListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    popupMenu.ShowAt((int) event.getX(), (int) event.getY(), event);
+                    return false;
+                case MotionEvent.ACTION_MOVE:
+                    return true;
+                case MotionEvent.ACTION_UP:
+                    popupMenu.setVisibility(View.GONE);
+                    return true;
+                default:
+                    return true;
+            }
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
